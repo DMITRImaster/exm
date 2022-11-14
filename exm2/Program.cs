81916ff39dc0248744e337743b441af3618b98ea
Dmitriy <dtm@gmeil.com>
2022-11-14 12:08:27 +0300

    exm2

---
+++ exm2/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Console.WriteLine("Enter number1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2) 
+{
+    Console.WriteLine("Первое число квадрат второг");
+}
+
+else 
+{
+    Console.WriteLine("Первое число не является квадрат второг");
+}
+*/
+ //int resalt=number * number;
+ //Console.WriteLine($"квкдрат числа {number} = {resalt}"); 
+
+ /*Console.WriteLine("Enter number");
+ int number = Convert.ToInt32(Console.ReadLine());
+ int index = (-1) * number;
+ while (index<=number)
+ {
+    Console.Write(index + " ");
+    index++;
+ }
+ */
+ /*
+ Console.WriteLine("Enter number");
+ int number = Convert.ToInt32(Console.ReadLine());
+ if (number>99 && number<1000)
+ {
+ Console.Write( "последнее число = " + number%10) ;
+ }
+ else {Console.Write( "Число не трехзначное " );}
+
+
+Console.WriteLine("Enter number");
+ int number = Convert.ToInt32(Console.ReadLine());*/
+Console.WriteLine("enter name");
+string username=Console.ReadLine();
+ Console.WriteLine("Привет,");
+ Console.Write(username);
